c2f892aedfd89779626682b1ec60aa73e7d9dd55
Steven Hanson <steven.hanson@wintechnologies.net>
2019-11-26 17:35:13 +0000

    Add backdrop to sidebar

--- template/src/App.js
+++ template/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import Backdrop from './components/Backdrop/Backdrop';
 import Sidebar from './components/Sidebar/Sidebar';
 import Toolbar from './components/Toolbar/Toolbar';
 
@@ -8,6 +9,7 @@ function App() {
     <div style={{height: '100%'}}>
       <Toolbar/>
       <Sidebar/>
+      <Backdrop/>
       <main style={{marginTop: '64px'}}>
         <p>This is the page content!</p>
       </main>
