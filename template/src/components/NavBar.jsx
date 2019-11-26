d7d2e2d74847ce47f17be723c3fe53545cf71029
Steven Hanson <steven.hanson@wintechnologies.net>
2019-11-26 15:37:53 +0000

    Add styles for navbar using style jsx

--- template/src/components/NavBar.jsx
+++ template/src/components/NavBar.jsx
@@ -4,12 +4,33 @@ import { Link } from 'react-router-dom';
 class NavBar extends Component {
   render() {
     return (
-      <header>
-        <ul id="headerButtons">
-          <li className="navButton"><Link to="">Home</Link></li>
-        </ul>
-      </header>
+      <>
+        <header>
+          <ul id="headerButtons">
+            <li className="navButton"><Link to="">Home</Link></li>
+          </ul>
+        </header>
+        <style jsx>
+          {`
+            header {
+              display: block;
+              background-color: black;
+              width: 100%;
+              height: 50px;
+              color: white;
+            }
+            #headerButtons {
+              list-style-type: none;
+              margin: 0;
+              padding: 0;
+            }
+            .navButton {
+              float: right;
+            }
+          `}
+        </style>
+      </>
     )
-  }
+}
 }
 export default NavBar;
